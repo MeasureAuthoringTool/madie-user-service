715536142493779f23c31983a9060265ed4a6672
nmorasb <nate.moraca@semanticbits.com>
2025-11-18 13:59:13 -0500

    MAT-9147: add retry support to the getToken call to make token updates more likely to succeed

--- src/test/java/gov/cms/madie/user/UserServiceApplicationTests.java
+++ src/test/java/gov/cms/madie/user/UserServiceApplicationTests.java
@@ -1,30 +1,38 @@
 package gov.cms.madie.user;
 
+import gov.cms.madie.user.services.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 @ActiveProfiles("test")
-// @SpringBootTest
+@SpringBootTest
 class UserServiceApplicationTests {
-  //  @Autowired private UserService userService;
+    @Autowired
+    private UserService userService;
 
-  @Test
-  void contextLoads() {
-    // given
-    // nothing to set up
-    // when
-    // nothing to call
-    // then
-    // context loads if no exception is thrown
-  }
+    @Test
+    void contextLoads() {
+        // given
+        // nothing to set up
+        // when
+        // nothing to call
+        // then
+        // context loads if no exception is thrown
+    }
 
-  @Test
-  void userServiceBeanIsLoaded() {
-    // given
-    // nothing to set up
-    // when
-    // nothing to call
-    // then
-    //    assertThat(userService, is(notNullValue()));
-  }
+    @Test
+    void userServiceBeanIsLoaded() {
+        // given
+        // nothing to set up
+        // when
+        // nothing to call
+        // then
+        assertThat(userService, is(notNullValue()));
+    }
 }
