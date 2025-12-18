a631d13fafe3e6b7f4a335ef437fcb04c37bc208
Greg Akins <gregory.akins@icf.com>
2025-12-18 09:07:54 -0500

    MAT-9095: Stupid imports

--- src/main/java/gov/cms/madie/user/services/UserService.java
+++ src/main/java/gov/cms/madie/user/services/UserService.java
@@ -10,6 +10,7 @@ import gov.cms.madie.user.dto.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
