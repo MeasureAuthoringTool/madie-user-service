90a5073b66f2db2f747dc26d8d00ef1cc4de7bbb
sb-cecilialiu <cecilia.liu@icf.com>
2026-03-24 13:26:29 -0500

    MAT-9632 add active

--- src/main/java/gov/cms/madie/user/services/UserService.java
+++ src/main/java/gov/cms/madie/user/services/UserService.java
@@ -92,6 +92,7 @@ public class UserService {
                     .email(user.getEmail())
                     .firstName(user.getFirstName())
                     .lastName(user.getLastName())
+                    .active(UserStatus.ACTIVE.equals(user.getStatus()))
                     .build())
         .orElse(null);
   }
