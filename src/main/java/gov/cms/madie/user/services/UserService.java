e4e3971a0ca57ee1f6e48b36dff8cd0b8d3024a7
adongare <ashok.dongare@semanticbits.com>
2026-07-14 12:29:10 -0400

    MAT-10232 revert local change

--- src/main/java/gov/cms/madie/user/services/UserService.java
+++ src/main/java/gov/cms/madie/user/services/UserService.java
@@ -43,17 +43,13 @@ public class UserService {
   }
 
   public MadieUser getUserByHarpId(String harpId) {
-    return MadieUser.builder()
-        .harpId(harpId)
-        .roles(List.of(HarpRole.builder().role("MADIE-ADMIN").build()))
-        .build();
-    //    return userRepository
-    //        .findByHarpId(StringUtils.toRootLowerCase(harpId))
-    //        .orElseGet(
-    //            () -> {
-    //              log.warn("User not found in database for HARP ID: {}", harpId);
-    //              return MadieUser.builder().harpId(harpId).build();
-    //            });
+    return userRepository
+      .findByHarpId(StringUtils.toRootLowerCase(harpId))
+      .orElseGet(
+        () -> {
+          log.warn("User not found in database for HARP ID: {}", harpId);
+          return MadieUser.builder().harpId(harpId).build();
+        });
   }
 
   public MadieUser refreshUserRolesAndLogin(String harpId) {
